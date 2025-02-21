8ba7f2027e6d26164624da289b8498472f4f22c3
kranthiranadev <kranthimodam@gmail.com>
2025-02-21 20:04:12 +0530

    Move TrackingPage into React

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import {Routes, Route} from 'react-router'
-import { HomePage } from './Pages/HomePage'
-import { CheckoutPage } from './Pages/CheckoutPage'
-import { OrdersPage } from './Pages/OrdersPage'
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./Pages/HomePage";
+import { CheckoutPage } from "./Pages/CheckoutPage";
+import { OrdersPage } from "./Pages/OrdersPage";
+import { TrackingPage } from "./Pages/TrackingPage";
+import "./App.css";
 
 function App() {
-
   return (
-    <Routes>  
+    <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
       <Route path="/orders" element={<OrdersPage />} />
-
+      <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
